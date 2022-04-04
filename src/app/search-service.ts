477ddc1c985e9b296de7bc9b1eb8ff71afaec997
leah_glazel <leah_glazel@PJ-LEAHGL>
2022-04-04 12:22:04 +0300

    fix

--- src/app/search-service.ts
+++ src/app/search-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
     providedIn: 'root'
@@ -10,10 +11,10 @@ export class SearchService{
     }
 
     getData(text: string, start: number): Observable<any>{
-        return this.http.get(`https://localhost:7008/api/Search/getData?searchText=${text}&start=${start}`);
+        return this.http.get(`${environment.API_ENDPOINT}Search/getData?searchText=${text}&start=${start}`);
     }
 
     getCountData(text: string): Observable<any>{
-        return this.http.get(`https://localhost:7008/api/Search/getCountData?searchText=${text}`);
+        return this.http.get(`${environment.API_ENDPOINT}Search/getCountData?searchText=${text}`);
     }
 }
