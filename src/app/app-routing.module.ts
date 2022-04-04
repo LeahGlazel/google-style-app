4d58dcac1120eb5bcbb006ed59ade725da30a249
leah_glazel <leah_glazel@PJ-LEAHGL>
2022-04-04 12:58:39 +0300

    last fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchContainerComponent } from './search-container/search-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/search',
+    pathMatch: 'full'
+  },
+  {
+    path: 'search',
+    component: SearchContainerComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
